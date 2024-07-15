8dd24dcc61d91374f959d67deaa2ea6b6780943d
Maks <mshevkun.dev@gmail.com>
2024-07-15 13:55:50 -0400

    Removed css from App component, Add Rating an Accordion components to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      This is App component
+      <Rating />
+      <Accordion />
     </div>
   );
+}
+
+function Rating() {
+  return (
+    <div>
+      <Star />
+      <Star />
+      <Star />
+      <Star />
+      <Star />
+    </div>
+  );
+}
+
+function Accordion() {
+  return (
+    <div>
+      <h3>Menu</h3>
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+      </ul>
+    </div>
+  );
+}
+
+function Star() {
+  return <span>&#9733;</span>;
 }
 
 export default App;
